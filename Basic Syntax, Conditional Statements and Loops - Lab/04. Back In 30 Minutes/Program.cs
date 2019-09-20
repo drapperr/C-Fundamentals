deaf951d230aa63a6a8ad7c6e746608bc9f79eef
drapperr <drapperr@users.noreply.github.com>
2019-09-20 18:15:36 +0300

    update

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int totalMinutes = hours * 60 + minutes + 30;
+            int hoursAfter30min = totalMinutes / 60;
+            int minutesAfter30min = totalMinutes % 60;
+            if (hoursAfter30min >= 24)
+            {
+                hoursAfter30min -= 24;
+            }
+            Console.WriteLine($"{hoursAfter30min}:{minutesAfter30min:D2}");
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/06. Foreign Languages/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06._Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string laguage = string.Empty;
+            if (country == "England" || country == "USA")
+            {
+                laguage = "English";
+            }
+            else if (country == "Spain" || country == "Argentina" || country == "Mexico")
+            {
+                laguage = "Spanish";
+            }
+            else
+            {
+                laguage = "unknown";
+            }
+            Console.WriteLine(laguage);
+        }
+    }
+}
