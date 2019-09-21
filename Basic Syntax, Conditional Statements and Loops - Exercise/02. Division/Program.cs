f554944e1a9696075042dfebc53b59cbdc1648c0
drapperr <drapperr@users.noreply.github.com>
2019-09-21 20:54:03 +0300

    update

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/02. Division/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02._Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2, 3, 6, 7, 10
+            int number = int.Parse(Console.ReadLine());
+            int result = 0;
+            if (number % 10 == 0)
+            {
+                result = 10;
+            }
+            else if (number % 7 == 0)
+            {
+                result = 7;
+            }
+            else if (number % 6 == 0)
+            {
+                result = 6;
+            }
+            else if (number % 3 == 0)
+            {
+                result = 3;
+            }
+            else if (number % 2 == 0)
+            {
+                result = 2;
+            }
+            if (result == 0)
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {result}");
+            }
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/03. Vacation/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            string groupType = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double price = 0;
+            double total = 0;
+            if (groupType == "Students")
+            {
+                if (dayOfWeek == "Friday")
+                {
+                    price = 8.45;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    price = 9.80;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    price = 10.46;
+                }
+            }
+            if (groupType == "Business")
+            {
+                if (dayOfWeek == "Friday")
+                {
+                    price = 10.90;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    price = 15.60;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    price = 16;
+                }
+            }
+            if (groupType == "Regular")
+            {
+                if (dayOfWeek == "Friday")
+                {
+                    price = 15;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    price = 20;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    price = 22.50;
+                }
+            }
+            total = people * price;
+            if (groupType == "Students" && people >= 30)
+            {
+                total -= total * 0.15;
+            }
+            if (groupType == "Business" && people >= 100)
+            {
+                total -= 10 * price;
+            }
+            if (groupType == "Regular" && people >= 10 && people <= 20)
+            {
+                total -= total * 0.05;
+            }
+            Console.WriteLine($"Total price: {total:f2}");
+        }
+    }
+}
