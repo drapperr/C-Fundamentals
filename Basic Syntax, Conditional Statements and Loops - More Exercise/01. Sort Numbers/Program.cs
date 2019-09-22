8d7dcc40db35a01885ec3902d7df2724f9b802b9
drapperr <drapperr@users.noreply.github.com>
2019-09-22 20:23:08 +0300

    update

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int firstNumber, secondNumber, thirdNumber;
+            if (n1 > n2 && n1 > n3)
+            {
+                firstNumber = n1;
+                if (n2 > n3)
+                {
+                    secondNumber = n2;
+                    thirdNumber = n3;
+                }
+                else
+                {
+                    secondNumber = n3;
+                    thirdNumber = n2;
+                }
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                firstNumber = n2;
+                if (n1 > n3)
+                {
+                    secondNumber = n1;
+                    thirdNumber = n3;
+                }
+                else
+                {
+                    secondNumber = n3;
+                    thirdNumber = n1;
+                }
+            }
+            else
+            {
+                firstNumber = n3;
+                if (n1 > n2)
+                {
+                    secondNumber = n1;
+                    thirdNumber = n2;
+                }
+                else
+                {
+                    secondNumber = n2;
+                    thirdNumber = n1;
+                }
+            }
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+            Console.WriteLine(thirdNumber);
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/02. English Name of the Last Digit/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _02._English_Name_of_the_Last_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int lastDigit = number % 10;
+            string result = string.Empty;
+            if (lastDigit == 0)
+            {
+                result = "zero";
+            }
+            else if (lastDigit == 1)
+            {
+                result = "one";
+            }
+            else if (lastDigit == 2)
+            {
+                result = "two";
+            }
+            else if (lastDigit == 3)
+            {
+                result = "three";
+            }
+            else if (lastDigit == 4)
+            {
+                result = "four";
+            }
+            else if (lastDigit == 5)
+            {
+                result = "five";
+            }
+            else if (lastDigit == 6)
+            {
+                result = "six";
+            }
+            else if (lastDigit == 7)
+            {
+                result = "seven";
+            }
+            else if (lastDigit == 8)
+            {
+                result = "eight";
+            }
+            else if (lastDigit == 9)
+            {
+                result = "nine";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
