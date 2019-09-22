4e54670b8d8e10f3f2a4c04fc0ea59f8e0eb983d
drapperr <drapperr@users.noreply.github.com>
2019-09-22 20:19:29 +0300

    update

--- Basic Syntax, Conditional Statements and Loops - Exercise/09. Padawan Equipment/Program.cs
+++ Basic Syntax, Conditional Statements and Loops - Exercise/09. Padawan Equipment/Program.cs
@@ -6,7 +6,27 @@ namespace _09._Padawan_Equipment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double moneyAmount = double.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            double lightsabersPrice = double.Parse(Console.ReadLine());
+            double robesPrice = double.Parse(Console.ReadLine());
+            double beltsPrice = double.Parse(Console.ReadLine());
+            int lightsabersCount = (int)Math.Ceiling(studentsCount * 1.1);
+            int beltsCount = studentsCount;
+            if (beltsCount >= 6)
+            {
+                beltsCount -= studentsCount / 6;
+            }
+            int robesCount = studentsCount;
+            double total = lightsabersPrice * lightsabersCount + robesPrice * robesCount + beltsPrice * beltsCount;
+            if (total <= moneyAmount)
+            {
+                Console.WriteLine($"The money is enough - it would cost {total:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {total - moneyAmount:f2}lv more.");
+            }
         }
     }
 }
--- Basic Syntax, Conditional Statements and Loops - Exercise/10. Rage Expenses/Program.cs
+++ Basic Syntax, Conditional Statements and Loops - Exercise/10. Rage Expenses/Program.cs
@@ -6,7 +6,40 @@ namespace _10._Rage_Expenses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+            int headsetCount = 0;
+            int mouseCount = 0;
+            int keyboardCount = 0;
+            int displayCount = 0;
+            for (int i = 1; i <= lostGames; i++)
+            {
+                bool trahsHeadset = false;
+                bool trahsMouse = false;
+                if (i % 2 == 0)
+                {
+                    headsetCount++;
+                    trahsMouse = true;
+                }
+                if (i % 3 == 0)
+                {
+                    mouseCount++;
+                    trahsHeadset = true;
+                }
+                if (trahsMouse && trahsHeadset)
+                {
+                    keyboardCount++;
+                    if (keyboardCount % 2 == 0)
+                    {
+                        displayCount++;
+                    }
+                }
+            }
+            double total = headsetPrice * headsetCount + mousePrice * mouseCount + keyboardCount * keyboardPrice + displayCount * displayPrice;
+            Console.WriteLine($"Rage expenses: {total:f2} lv.");
         }
     }
 }
