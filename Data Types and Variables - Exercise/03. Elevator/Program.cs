a67f011d4af403ed7c4c31060a414c0ff20a8ec5
drapperr <drapperr@users.noreply.github.com>
2019-09-26 20:10:52 +0300

    update

---
+++ Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling(peopleCount / (double)capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
