d8290a388962f01ef940bf4fb446c048ab7dd946
drapperr <drapperr@users.noreply.github.com>
2019-09-27 22:09:44 +0300

    update

---
+++ Data Types and Variables - More Exercise/05. Decrypting Message/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05._Decrypting_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                char code = char.Parse(Console.ReadLine());
+                message += (char)(code + key);
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
