017166862c8359c662fec4ee463dbc2426ec92db
drapperr <drapperr@users.noreply.github.com>
2019-09-27 22:08:55 +0300

    update

---
+++ Data Types and Variables - More Exercise/02. From Left to The Right/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _02._From_Left_to_The_Right
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] nums = input.Split();
+                BigInteger firstNum = BigInteger.Parse(nums[0]);
+                BigInteger secondNum = BigInteger.Parse(nums[1]);
+                BigInteger biggerNummber;
+                int sum = 0;
+                if (firstNum > secondNum)
+                {
+                    biggerNummber = firstNum;
+                }
+                else
+                {
+                    biggerNummber = secondNum;
+                }
+                if (biggerNummber < 0)
+                {
+                    biggerNummber *= -1;
+                }
+                while (biggerNummber != 0)
+                {
+                    BigInteger digit = biggerNummber % 10;
+                    sum += (int)digit;
+                    biggerNummber /= 10;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
