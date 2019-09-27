2281cb071432f57501e3637c9eb2b177920b4013
drapperr <drapperr@users.noreply.github.com>
2019-09-27 22:09:09 +0300

    update

---
+++ Data Types and Variables - More Exercise/03. Floating Equality/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            a /= 0.000001;
+            b /= 0.000001;
+            Console.WriteLine((int)a == (int)b);
+        }
+    }
+}
