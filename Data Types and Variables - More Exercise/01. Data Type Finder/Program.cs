7e9b6d32894d54abe8c54d245e407d2d8f1eae26
drapperr <drapperr@users.noreply.github.com>
2019-09-27 22:08:40 +0300

    update

---
+++ Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                bool isBolean = bool.TryParse(input, out bool boolean);
+                bool isInteger = int.TryParse(input, out int integer);
+                bool isChar = char.TryParse(input, out char character);
+                bool isDouble = double.TryParse(input, out double floating);
+                if (isBolean)
+                {
+                    Console.WriteLine($"{input} is boolean type");
+
+                }
+                else if (isInteger)
+                {
+                    Console.WriteLine($"{input} is integer type");
+
+                }
+                else if (isChar)
+                {
+                    Console.WriteLine($"{input} is character type");
+
+                }
+                else if (isDouble)
+                {
+                    Console.WriteLine($"{input} is floating point type");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+            }
+        }
+    }
+}
