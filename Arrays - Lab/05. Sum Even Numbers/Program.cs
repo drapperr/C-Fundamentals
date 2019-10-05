a3536161d9be88550d982fc2db4fbfeca23ed134
drapperr <drapperr@users.noreply.github.com>
2019-10-05 22:12:24 +0300

    update

---
+++ Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split().ToArray();
+            int sum = 0;
+            foreach (var num in numbers)
+            {
+                int currentNumber = int.Parse(num);
+                if (currentNumber % 2 == 0)
+                {
+                    sum += currentNumber;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
