f3bfe947b8920698c6250fbc77719964aa02ea82
drapperr <drapperr@users.noreply.github.com>
2019-10-06 22:15:34 +0300

    update

---
+++ Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int[] peopleOnWagon = new int[wagons];
+            int sum = 0;
+            for (int i = 0; i < wagons; i++)
+            {
+                peopleOnWagon[i] = int.Parse(Console.ReadLine());
+                sum += peopleOnWagon[i];
+            }
+            Console.WriteLine(string.Join(" ", peopleOnWagon));
+            Console.WriteLine(sum);
+        }
+    }
+}
