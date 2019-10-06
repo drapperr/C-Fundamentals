258bb60df3d9fe50a524f831729649aa7e8ec112
drapperr <drapperr@users.noreply.github.com>
2019-10-06 22:17:34 +0300

    update

---
+++ Arrays - Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            string biggerNums = string.Empty;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool bigger = true;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        bigger = false;
+                    }
+                }
+                if (bigger)
+                {
+                    biggerNums += numbers[i] + " ";
+                }
+            }
+            Console.WriteLine(biggerNums);
+        }
+    }
+}
