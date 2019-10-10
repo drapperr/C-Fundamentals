71e33bc06415f27a1266b5e702451cdce3600e3c
drapperr <drapperr@users.noreply.github.com>
2019-10-10 21:29:48 +0300

    update

---
+++ Arrays - More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] triangleLine = { 1 };
+            int[] oldTriangleLine = triangleLine;
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(string.Join(" ", triangleLine));
+                triangleLine = new int[i + 1];
+                triangleLine[0] = 1;
+                triangleLine[triangleLine.Length - 1] = 1;
+                for (int j = 1; j < triangleLine.Length - 1; j++)
+                {
+                    triangleLine[j] = oldTriangleLine[j - 1] + oldTriangleLine[j];
+                }
+                oldTriangleLine = triangleLine;
+            }
+        }
+    }
+}
