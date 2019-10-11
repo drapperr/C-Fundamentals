012b86a49c6e1b0a2b55c381b8d2b36f4df4c95d
drapperr <drapperr@users.noreply.github.com>
2019-10-11 22:05:31 +0300

    update

---
+++ Arrays - More Exercise/05. Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Longest_Increasing_Subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            List<int> lis = new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                List<int> currentLis = new List<int>();
+                currentLis.Add(numbers[i]);
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j]>currentLis[currentLis.Count-1])
+                    {
+                        currentLis.Add(numbers[j]);
+                    }
+                }
+                if (currentLis.Count>lis.Count)
+                {
+                    lis = new List<int>(currentLis);
+                }
+            }
+            Console.WriteLine(string.Join(" ",lis));
+        }
+    }
+}
