f9a309ee3d5ad3b4cbd4cd5eb9259ee63aaf90c9
drapperr <drapperr@users.noreply.github.com>
2019-10-12 17:30:08 +0300

    update

---
+++ Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine(Area(width, height));
+        }
+
+        private static int Area(int width, int height)
+        {
+            return width * height;
+        }
+    }
+}
