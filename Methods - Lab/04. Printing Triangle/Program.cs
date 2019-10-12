11955490f14fd7347ec3fcebbe9ce811432ae364
drapperr <drapperr@users.noreply.github.com>
2019-10-12 17:29:25 +0300

    update

---
+++ Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintFirstPart(number);
+            PrintSecondPart(number);
+        }
+
+        private static void PrintFirstPart(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                PrintLine(i);
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrintLine(int i)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+        }
+
+        private static void PrintSecondPart(int number)
+        {
+            for (int i = number - 1; i > 0; i--)
+            {
+                PrintLine(i);
+                Console.WriteLine();
+            }
+        }
+    }
+}
