046840a93e62cd82fa75b80a61baab7296212387
drapperr <drapperr@users.noreply.github.com>
2019-10-12 17:30:39 +0300

    update

---
+++ Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            PrintResult(text, count);
+        }
+
+        private static void PrintResult(string text, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(text);
+            }
+        }
+    }
+}
