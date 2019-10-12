1d9c9e0d6f116fb73aba8e16985b1f1c51681f2d
drapperr <drapperr@users.noreply.github.com>
2019-10-12 17:30:58 +0300

    update

---
+++ Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintResult(number, power));
+        }
+
+        private static double PrintResult(double number, int power)
+        {
+            double result = 1.0;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+    }
+}
