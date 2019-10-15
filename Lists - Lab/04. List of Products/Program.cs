13fb8b12534918b94c521903d938d3e0d0d47857
drapperr <drapperr@users.noreply.github.com>
2019-10-15 17:45:01 +0300

    update

---
+++ Lists - Lab/04. List of Products/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int productsCount = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < productsCount; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                products.Add(currentProduct);
+            }
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
