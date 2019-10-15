08a19ab7e1ee84048c76a51e0250e1b1ba00cfc4
drapperr <drapperr@users.noreply.github.com>
2019-10-15 17:45:47 +0300

    update

---
+++ Lists - Lab/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._List_Manipulation_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                List<string> currentCommand = command.Split().ToList();
+                string action = currentCommand[0];
+                int number = int.Parse(currentCommand[1]);
+                if (action == "Add")
+                {
+                    numbers.Add(number);
+                }
+                else if (action == "Remove")
+                {
+                    numbers.Remove(number);
+                }
+                else if (action == "RemoveAt")
+                {
+                    int index = number;
+                    numbers.RemoveAt(index);
+                }
+                else if (action == "Insert")
+                {
+                    int index = int.Parse(currentCommand[2]);
+                    numbers.Insert(index, number);
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
