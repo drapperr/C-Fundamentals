621e6a1408d271ef10cb19a752202b0b2094609c
drapperr <drapperr@users.noreply.github.com>
2019-10-16 21:05:33 +0300

    update

---
+++ Methods - Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            GetMinNumber(a, b, c);
+        }
+
+        private static void GetMinNumber(int a, int b, int c)
+        {
+            int minNumber = int.MaxValue;
+            if (a < minNumber)
+            {
+                minNumber = a;
+            }
+            if (b < minNumber)
+            {
+                minNumber = b;
+            }
+            if (c < minNumber)
+            {
+                minNumber = c;
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
