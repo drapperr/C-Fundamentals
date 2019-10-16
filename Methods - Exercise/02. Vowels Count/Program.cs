7cccf1864da0656b8d80585f00582dd64462df71
drapperr <drapperr@users.noreply.github.com>
2019-10-16 21:06:17 +0300

    update

---
+++ Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            GetVowelsCount(text);
+        }
+
+        private static void GetVowelsCount(string text)
+        {
+            int volwelsSum = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u')
+                {
+                    volwelsSum++;
+                }
+            }
+            Console.WriteLine(volwelsSum);
+        }
+    }
+}
