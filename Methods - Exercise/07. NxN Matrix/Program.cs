344c6d42bedcdfffb0e4ff846bbaf3b8628ca373
drapperr <drapperr@users.noreply.github.com>
2019-10-19 19:42:19 +0300

    update

---
+++ Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);
+        }
+
+        private static void PrintMatrix(int n)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                PrntCols(n);
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrntCols(int n)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                Console.Write($"{n} ");
+            }
+        }
+    }
+}
