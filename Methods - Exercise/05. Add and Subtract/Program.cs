681ca9352549dd8c4eabe8b4cca8a38bde87daf6
drapperr <drapperr@users.noreply.github.com>
2019-10-19 19:41:09 +0300

    update

---
+++ Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int sum = SumNumbers(firstNumber, secondNumber);
+            Console.WriteLine(Subtract(sum, thirdNumber));
+        }
+
+        private static int Subtract(int sum, int thirdNumber)
+        {
+            return sum - thirdNumber;
+        }
+
+        private static int SumNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
