1cd185f271df9c7bf2678e7a6549648d7c67f748
drapperr <drapperr@users.noreply.github.com>
2019-10-19 19:41:47 +0300

    update

---
+++ Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string midCharacters = GetMidCharacter(text);
+            Console.WriteLine(midCharacters);
+        }
+
+        private static string GetMidCharacter(string text)
+        {
+            if (text.Length % 2 == 0)
+            {
+                string firstChar = text[(text.Length - 1) / 2].ToString();
+                string secondChar = text[((text.Length - 1) / 2) + 1].ToString();
+                return firstChar + secondChar;
+            }
+            else
+            {
+                return text[(text.Length - 1) / 2].ToString();
+            }
+        }
+    }
+}
