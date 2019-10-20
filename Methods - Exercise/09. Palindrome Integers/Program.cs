cc14a5074f4ac8936dc6455913106dc3eb27f8fe
drapperr <drapperr@users.noreply.github.com>
2019-10-20 17:03:34 +0300

    update

---
+++ Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            while ((number = Console.ReadLine()) != "END")
+            {
+                if (ChekPalindrom(number))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+
+        private static bool ChekPalindrom(string number)
+        {
+            bool isPalindrom = true;
+            for (int i = 0; i < number.Length / 2; i++)
+            {
+                if (number[i] != number[number.Length - 1 - i])
+                {
+                    isPalindrom = false;
+                }
+            }
+            return isPalindrom;
+        }
+    }
+}
