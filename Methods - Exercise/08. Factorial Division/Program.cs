b2bf540757c473264a251fa43db757968a2825a4
drapperr <drapperr@users.noreply.github.com>
2019-10-20 17:03:07 +0300

    update

---
+++ Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{Factorial(firstNumber) / Factorial(secondNumber):F2}");
+        }
+
+        private static double Factorial(int number)
+        {
+            double result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
