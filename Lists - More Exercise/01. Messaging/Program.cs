3bc0d9f2ce3417087ba4dfd434b39feb6279ce77
drapperr <drapperr@users.noreply.github.com>
2019-11-05 18:05:46 +0200

    update

---
+++ Lists - More Exercise/01. Messaging/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<char> text = Console.ReadLine().ToList();
+            string result = string.Empty;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int index = 0;
+                while (numbers[i] > 0)
+                {
+                    index += numbers[i] % 10;
+                    numbers[i] /= 10;
+                }
+                while (index >= text.Count)
+                {
+                    index -= text.Count;
+                }
+                result += text[index];
+                text.RemoveAt(index);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
