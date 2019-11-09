046ea1a8589c42a990218d1b064ad5b3042f50c7
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-09 19:45:43 +0200

    update

---
+++ Methods - More Exercise/04. Tribonacci Sequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04._Tribonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            GetTribonacci(count);
+        }
+
+        private static void GetTribonacci(int count)
+        {
+            int firstNum = 0;
+            int secondNum = 0;
+            int thirdNum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int triboNum = firstNum + secondNum + thirdNum;
+                if (i == 0)
+                {
+                    triboNum = 1;
+                }
+                Console.Write(triboNum + " ");
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                thirdNum = triboNum;
+            }
+        }
+    }
+}
