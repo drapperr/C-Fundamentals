1f797ee72bb7b3287df2d5bbbbe2cceeeaafb7b4
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-09 19:43:09 +0200

    update

---
+++ Methods - More Exercise/01. Data Types/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string number = Console.ReadLine();
+            switch (type)
+            {
+                case "int": IntOperation(number); break;
+                case "real": RealOperation(number); break;
+                case "string": StringOperation(number); break;
+            }
+        }
+
+        private static void StringOperation(string number)
+        {
+            Console.WriteLine($"${number}$");
+        }
+
+        private static void RealOperation(string number)
+        {
+            Console.WriteLine($"{double.Parse(number) * 1.5:F2}");
+        }
+
+        private static void IntOperation(string number)
+        {
+            Console.WriteLine(int.Parse(number) * 2);
+        }
+    }
+}
