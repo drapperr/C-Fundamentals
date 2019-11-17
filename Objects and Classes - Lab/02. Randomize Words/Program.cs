d26d9922828a25462b96501c5dd3183882d14ff3
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-17 16:42:21 +0200

    update

---
+++ Objects and Classes - Lab/02. Randomize Words/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split();
+            Random rnd = new Random();
+            for (int i = 0; i < text.Length; i++)
+            {
+                int randomIndex = rnd.Next(0, text.Length);
+                string temp = text[i];
+                text[i] = text[randomIndex];
+                text[randomIndex] = temp;
+            }
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Objects and Classes - Lab/03. Big Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace _03._Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger bigFactorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                bigFactorial *= i;
+            }
+            Console.WriteLine(bigFactorial);
+        }
+    }
+}
