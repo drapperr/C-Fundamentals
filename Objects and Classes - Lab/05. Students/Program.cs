2589ee597005fde36a46db5559edd358dabb6d13
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-17 16:42:50 +0200

    update

---
+++ Objects and Classes - Lab/05. Students/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentsList = new List<Student>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] splitedInput = input.Split();
+
+                Student student = new Student();
+
+                student.FirstName = splitedInput[0];
+                student.LastName = splitedInput[1];
+                student.Age = splitedInput[2];
+                student.HomeTown = splitedInput[3];
+
+                studentsList.Add(student);
+            }
+            string cityName = Console.ReadLine();
+
+            foreach (var student in studentsList)
+            {
+                if (student.HomeTown == cityName)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
---
+++ Objects and Classes - Lab/06. Students 2.0/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentsList = new List<Student>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] splitedInput = input.Split();
+
+                Student student = new Student();
+
+                student.FirstName = splitedInput[0];
+                student.LastName = splitedInput[1];
+                student.Age = splitedInput[2];
+                student.HomeTown = splitedInput[3];
+
+                RemoveIfSutdentIsExisting(studentsList, student);
+
+                studentsList.Add(student);
+            }
+            string cityName = Console.ReadLine();
+
+            foreach (var student in studentsList)
+            {
+                if (student.HomeTown == cityName)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+
+        private static void RemoveIfSutdentIsExisting(List<Student> studentsList, Student student)
+        {
+            foreach (var studentInList in studentsList)
+            {
+                if (student.FirstName == studentInList.FirstName
+                    && student.LastName == studentInList.LastName)
+                {
+                    studentsList.Remove(studentInList);
+                    break;
+                }
+            }
+        }
+    }
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
