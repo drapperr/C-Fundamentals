bcdeb51ae2b363915c57af9f87783762d597df23
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-17 16:41:51 +0200

    update

---
+++ Objects and Classes - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split("-")
+                .Select(int.Parse)
+                .ToArray();
+            int day = input[0];
+            int month = input[1];
+            int year = input[2];
+            DateTime currentDate = new DateTime(year, month, day);
+            Console.WriteLine(currentDate.DayOfWeek);
+        }
+    }
+}
