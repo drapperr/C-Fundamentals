21e9979c8f77758c7d435582f7577e1bb11709f0
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-18 12:35:35 +0200

    update

---
+++ Objects and Classes - More Exercise/01. Company Roster/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Company_Roster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            int employeesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < employeesCount; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                double salary = double.Parse(input[1]);
+                string department = input[2];
+
+                Employee employee = new Employee(name, salary, department);
+
+                employees.Add(employee);
+            }
+            List<Employee> coppyemployees = employees.ToList();
+
+            double bestSumSalary = 0;
+            string bestDepartment = string.Empty;
+
+            while (coppyemployees.Count != 0)
+            {
+                double currentSumSalary = 0;
+                string currentDepartment = coppyemployees[0].Department;
+                for (int i = 0; i < coppyemployees.Count; i++)
+                {
+                    if (coppyemployees[i].Department == currentDepartment)
+                    {
+                        currentSumSalary += coppyemployees[i].Salary;
+                        coppyemployees.RemoveAt(i);
+                        i--;
+                    }
+                }
+                if (currentSumSalary > bestSumSalary)
+                {
+                    bestSumSalary = currentSumSalary;
+                    bestDepartment = currentDepartment;
+                }
+            }
+            employees = employees.Where(x => x.Department == bestDepartment).OrderByDescending(x => x.Salary).ToList();
+
+            Console.WriteLine($"Highest Average Salary: {bestDepartment}");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.Name} {employee.Salary:f2}");
+            }
+        }
+    }
+    class Employee
+    {
+        public Employee(string name, double salary, string department)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Department = department;
+        }
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+        public string Department { get; set; }
+
+    }
+}
---
+++ Objects and Classes - More Exercise/02. Oldest Family Member/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Oldest_Family_Member
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int membersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < membersCount; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                family.AddMember(name, age);
+            }
+            family.GetOldestMember();
+        }
+    }
+    class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+        public void AddMember(string name, int age)
+        {
+            People.Add(new Person(name, age));
+        }
+        public void GetOldestMember()
+        {
+            People = People.OrderByDescending(x => x.Age).ToList();
+            Console.WriteLine($"{People[0].Name} {People[0].Age}");
+        }
+    }
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
