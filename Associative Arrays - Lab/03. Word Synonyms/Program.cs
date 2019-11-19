1b2bb11b129bdb9b27a54796475d0c325b371635
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-19 16:58:49 +0200

    update

---
+++ Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordsCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> synonimList = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+
+                if (synonimList.ContainsKey(word))
+                {
+                    synonimList[word].Add(synonim);
+                }
+                else
+                {
+                    synonimList.Add(word, new List<string> { synonim });
+                }
+            }
+            foreach (var item in synonimList)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
