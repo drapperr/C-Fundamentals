ca86488b33afdfa4fdb45c45e5c0daddbc57f1ea
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-19 16:59:02 +0200

    update

---
+++ Associative Arrays - Lab/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .ToList();
+
+            int counter = 0;
+            List<int> result = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                if (counter < 3)
+                {
+                    result.Add(number);
+                    counter++;
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
