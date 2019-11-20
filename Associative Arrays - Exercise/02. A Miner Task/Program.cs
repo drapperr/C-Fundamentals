0f3f2114dc51c6c91c604b77a91e3b170334fedc
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-20 09:51:46 +0200

    update

---
+++ Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resucesQuantity = new Dictionary<string, int>();
+
+            string resurce;
+
+            while ((resurce = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resucesQuantity.ContainsKey(resurce))
+                {
+                    resucesQuantity[resurce] += quantity;
+                }
+                else
+                {
+                    resucesQuantity.Add(resurce, quantity);
+                }
+            }
+            foreach (var currentResurce in resucesQuantity)
+            {
+                Console.WriteLine($"{currentResurce.Key} -> {currentResurce.Value}");
+            }
+        }
+    }
+}
