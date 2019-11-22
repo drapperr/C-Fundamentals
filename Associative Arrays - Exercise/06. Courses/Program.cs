972cbd664bb0d471c17e8de319ce167694de29bb
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-22 07:53:00 +0200

    update

---
+++ Associative Arrays - Exercise/06. Courses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] splitedInput = input.Split(" : ");
+
+                string courseName = splitedInput[0];
+                string studantName = splitedInput[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses[courseName] = new List<string>();
+                }
+                courses[courseName].Add(studantName);
+            }
+            courses = courses.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                course.Value.Sort();
+                foreach (var student in course.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
