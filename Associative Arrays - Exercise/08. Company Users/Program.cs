493f5c78d3ba5021abfe860d88afcdea5ea549e4
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-22 07:53:27 +0200

    update

---
+++ Associative Arrays - Exercise/08. Company Users/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, List<string>> companyEmployees = new SortedDictionary<string, List<string>>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] splitedInput = input.Split(" -> ");
+
+                string companyName = splitedInput[0];
+                string employeeId = splitedInput[1];
+
+                if (!companyEmployees.ContainsKey(companyName))
+                {
+                    companyEmployees[companyName] = new List<string>();
+                }
+                if (!companyEmployees[companyName].Contains(employeeId))
+                {
+                    companyEmployees[companyName].Add(employeeId);
+                }
+            }
+            foreach (var company in companyEmployees)
+            {
+                Console.WriteLine(company.Key);
+                foreach (var id in company.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
