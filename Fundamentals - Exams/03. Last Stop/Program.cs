85500fc4f6d54f79dfe37ab68b68d2454c003a7e
DraPpeRr <drapperr@users.noreply.github.com>
2019-11-23 13:46:35 +0200

    update

---
+++ Fundamentals - Exams/03. Last Stop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Last_Stop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
