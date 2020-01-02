5970e2c7b9633ddba20fdd07391c174fc2c4757a
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-02 12:21:04 +0200

    update

---
+++ Fundamentals - Exams/Message Decrypter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Message_Decrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^(\$|%)(?<tag>[A-Z][a-z]{2,})\1:\s\[(?<first>\d+)\]\|\[(?<second>\d+)\]\|\[(?<third>\d+)\]\|$");
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (regex.IsMatch(input))
+                {
+                    Match match = regex.Match(input);
+
+                    string tag = match.Groups["tag"].Value;
+                    char firstChar = (char)int.Parse(match.Groups["first"].Value);
+                    char secondChar = (char)int.Parse(match.Groups["second"].Value);
+                    char thirdChar = (char)int.Parse(match.Groups["third"].Value);
+                    string message = firstChar.ToString() + secondChar + thirdChar;
+
+                    Console.WriteLine($"{tag}: {message}");
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+            }
+        }
+    }
+}
