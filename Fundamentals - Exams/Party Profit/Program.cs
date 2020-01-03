a62ed72811b9495755b105d8ff3f3ecfb3256917
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-03 16:07:17 +0200

    update

---
+++ Fundamentals - Exams/Party Profit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Party_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int partySize = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int coins = 0;
+
+            for (int day = 1; day <= days; day++)
+            {
+                if (day % 10 == 0)
+                {
+                    partySize -= 2;
+                }
+                if (day % 15 == 0)
+                {
+                    partySize += 5;
+                }
+                coins += 50;
+                coins -= 2 * partySize;
+                if (day % 3 == 0)
+                {
+                    coins -= 3 * partySize;
+                }
+                if (day % 5 == 0)
+                {
+                    coins += 20 * partySize;
+                    if (day % 3 == 0)
+                    {
+                        coins -= 2 * partySize;
+                    }
+                }
+            }
+            Console.WriteLine($"{partySize} companions received {coins / partySize} coins each.");
+        }
+    }
+}
