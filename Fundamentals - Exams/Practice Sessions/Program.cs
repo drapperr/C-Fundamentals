706849236ae4140566e097a0a135b01f167b6dde
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-04 13:42:48 +0200

    update

---
+++ Fundamentals - Exams/Practice Sessions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_Sessions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
