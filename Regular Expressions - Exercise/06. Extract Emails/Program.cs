94f7b6e0b3a9f01d5b9e6a85b537daae4795173f
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-09 15:18:36 +0200

    update

---
+++ Regular Expressions - Exercise/06. Extract Emails/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Extract_Emails
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string pattern = @"((?<=\s)[a-zA-Z0-9]+([-.]\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\.[a-z]{2,}))";
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(input);
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Groups[1]);
+        }
+    }
+}
