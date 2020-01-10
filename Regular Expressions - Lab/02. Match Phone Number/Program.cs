e37ed59c0fffb8a29402ab3ac00d9d7f8def2c88
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-10 13:19:30 +0200

    update

---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"\+359( |-)2\1\d{3}\1\d{4}\b");
+
+            string inputNumbers = Console.ReadLine();
+
+            var validNumbers = regex.Matches(inputNumbers);
+
+            Console.WriteLine(string.Join(", ", validNumbers));
+        }
+    }
+}
