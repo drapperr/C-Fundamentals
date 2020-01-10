d64e016ce5daed171f30df0dd3631f443288d650
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-10 13:18:56 +0200

    update

---
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+
+            string names = Console.ReadLine();
+
+            var matchedNames = regex.Matches(names);
+
+            foreach (var name in matchedNames)
+            {
+                Console.Write(name + " ");
+            }
+        }
+    }
+}
