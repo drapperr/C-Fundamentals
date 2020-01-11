f0fce0d7626d96065b7087bb8b4bbe3ab0856929
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-11 14:37:54 +0200

    update

---
+++ Regular Expressions - More Exercise/1. Winning Ticket/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1._Winning_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex separatorRegex = new Regex(@"[\s]*,[\s]+");
+            Regex jackpotRegex = new Regex(@"@{20}|#{20}|\${20}|\^{20}");
+            Regex winnerTicketRegex = new Regex(@"^[^@#$^]*(@{6,9}|#{6,9}|\${6,9}|\^{6,9})[^@#$^]*(\1)[^@#$^]*$");
+
+            string[] tickets = separatorRegex.Split(Console.ReadLine());
+
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Length == 20)
+                {
+                    if (jackpotRegex.IsMatch(ticket))
+                    {
+                        Console.WriteLine($"ticket \"{ticket}\" - 10{ticket[0]} Jackpot!");
+                    }
+                    else if (winnerTicketRegex.IsMatch(ticket))
+                    {
+                        string winSymbols = winnerTicketRegex.Match(ticket).Groups[1].Value;
+                        int countOfSymbols = winSymbols.Length;
+                        char winSymbol = winSymbols[0];
+                        Console.WriteLine($"ticket \"{ticket}\" - {countOfSymbols}{winSymbol}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{ticket}\" - no match");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid ticket");
+                }
+            }
+        }
+    }
+}
