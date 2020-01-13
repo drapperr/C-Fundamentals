81e7cba183c26998af886ff990132c093db95d0f
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-13 11:12:00 +0200

    update

---
+++ Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+
+            List<string> validPasswords = new List<string>();
+
+            foreach (var currentPassword in input)
+            {
+                bool validLendth = false;
+                bool validSybols = true;
+
+                if (currentPassword.Length >= 3 && currentPassword.Length <= 16)
+                {
+                    validLendth = true;
+                }
+
+                foreach (var symbol in currentPassword)
+                {
+                    if (!(char.IsLetterOrDigit(symbol)
+                          || symbol == '-' || symbol == '_'))
+                    {
+                        validSybols = false;
+                        break;
+                    }
+                }
+                if (validSybols && validLendth)
+                {
+                    validPasswords.Add(currentPassword);
+                }
+            }
+            foreach (var password in validPasswords)
+            {
+                Console.WriteLine(password);
+            }
+        }
+    }
+}
