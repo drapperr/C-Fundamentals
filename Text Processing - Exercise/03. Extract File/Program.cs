081aaf4eeffe6c6b3fba9e8330f7e215e4df5e6f
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-14 17:44:40 +0200

    update

---
+++ Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("\\");
+
+            string[] file = input[input.Length - 1].Split(".");
+
+            string fileName = file[0];
+            string extension = file[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
