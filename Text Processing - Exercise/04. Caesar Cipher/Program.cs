6fc172610582c97955c677f6a88217f41f94b0d8
DraPpeRr <drapperr@users.noreply.github.com>
2020-01-14 17:45:04 +0200

    update

---
+++ Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder encryptedText = new StringBuilder();
+
+            foreach (var symbol in text)
+            {
+                char encryptedSymbol = (char)(symbol + 3);
+                encryptedText.Append(encryptedSymbol);
+            }
+            Console.WriteLine(encryptedText);
+        }
+    }
+}
