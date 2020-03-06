1e197a0a9a30056d6b94fe52340186c0d303fcf7
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-06 12:01:42 +0200

    update

---
+++ Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            char lastChar = input[0];
+            ;
+            result.Append(input[0]);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == lastChar)
+                {
+                    continue;
+                }
+                else
+                {
+                    result.Append(input[i]);
+                    lastChar = input[i];
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
