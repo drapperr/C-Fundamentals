a972b437d9e7ba1e01c95815cca5ba9eee2079f8
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-09 15:09:55 +0200

    update

---
+++ Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace _07._String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            int bombStrength = 0;
+
+            foreach (var item in input)
+            {
+                if (char.IsDigit(item))
+                {
+                    bombStrength += int.Parse(item.ToString());
+                }
+                if (item == '>')
+                {
+                    result.Append(item);
+                }
+                else
+                {
+                    if (bombStrength == 0)
+                    {
+                        result.Append(item);
+                    }
+                    else
+                    {
+                        bombStrength--;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
