725dca221d443a482c181a206d6028b9914d0f19
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-09 15:10:17 +0200

    update

---
+++ Text Processing - Exercise/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _08._Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double totalSum = 0.0;
+
+            foreach (var code in input)
+            {
+                string numberString = string.Empty;
+
+                for (int i = 1; i < code.Length - 1; i++)
+                {
+                    numberString += code[i];
+                }
+                double number = double.Parse(numberString);
+                char firstChar = code[0];
+                char lastChar = code[code.Length - 1];
+
+                if (char.IsUpper(firstChar))
+                {
+                    int codeNumber = (int)(firstChar - 'A' + 1);
+                    number /= codeNumber;
+                }
+                else if (char.IsLower(firstChar))
+                {
+                    int codeNumber = (int)(firstChar - 'a' + 1);
+                    number *= codeNumber;
+                }
+
+                if (char.IsUpper(lastChar))
+                {
+                    int codeNumber = (int)(lastChar - 'A' + 1);
+                    number -= codeNumber;
+                }
+                else if (char.IsLower(lastChar))
+                {
+                    int codeNumber = (int)(lastChar - 'a' + 1);
+                    number += codeNumber;
+                }
+
+                totalSum += number;
+            }
+            Console.WriteLine($"{totalSum:F2}");
+        }
+    }
+}
