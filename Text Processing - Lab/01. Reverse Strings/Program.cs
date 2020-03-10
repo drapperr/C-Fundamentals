b101d20979ce0205159ad005169835fc78aa8c1a
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-10 10:37:09 +0200

    update

---
+++ Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText;
+
+            while ((inputText = Console.ReadLine()) != "end")
+            {
+                string reversedText = string.Empty;
+
+                for (int i = inputText.Length - 1; i >= 0; i--)
+                {
+                    reversedText += inputText[i];
+                }
+
+                Console.WriteLine($"{inputText} = {reversedText}");
+            }
+        }
+    }
+}
