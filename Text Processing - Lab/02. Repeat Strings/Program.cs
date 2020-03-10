9021f7ee7e4c19eb915de9216bd0c60bf5e39c46
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-10 10:37:54 +0200

    update

---
+++ Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputText = Console.ReadLine().Split();
+
+            string resultText = string.Empty;
+
+            foreach (var word in inputText)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    resultText += word;
+                }
+            }
+            Console.WriteLine(resultText);
+        }
+    }
+}
