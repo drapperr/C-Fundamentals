bc8c9ae90cb0f0647542ed498eab16ddd5f41e36
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-11 13:01:16 +0200

    update

---
+++ Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            string text = Console.ReadLine();
+
+            while (text.Contains(word))
+            {
+                text = text.Replace(word, string.Empty);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
