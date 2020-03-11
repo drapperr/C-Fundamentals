791fee5fb4c3e000ea0044011f1e29904c7199db
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-11 13:01:37 +0200

    update

---
+++ Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+
+            foreach (var word in words)
+            {
+                string replaceWord = new string('*', word.Length);
+
+                while (text.Contains(word))
+                {
+                    text = text.Replace(word, replaceWord);
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
