a6b05a5aac3365c968e3a373318a0d99497d01a2
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-11 13:02:01 +0200

    update

---
+++ Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string others = string.Empty;
+
+            foreach (var item in input)
+            {
+                if (char.IsDigit(item))
+                {
+                    digits += item;
+                }
+                else if (char.IsLetter(item))
+                {
+                    letters += item;
+                }
+                else
+                {
+                    others += item;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
