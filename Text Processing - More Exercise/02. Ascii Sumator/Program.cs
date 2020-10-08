6e3aa36701ece04fc411cdc9b66e4d0d10471556
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-09 00:47:48 +0300

    update

---
+++ Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            char minChar = (char)Math.Min(firstChar, secondChar);
+            char maxChar = (char)Math.Max(firstChar, secondChar);
+
+            int sum = 0;
+
+            foreach (var charachter in input)
+            {
+                if (charachter > minChar && charachter < maxChar)
+                {
+                    sum += charachter;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
