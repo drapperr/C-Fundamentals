cda094f3706e6e7498dd61611118adb77b739a12
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-10 20:08:25 +0300

    update

---
+++ Text Processing - More Exercise/04. Morse Code Translator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._Morse_Code_Translator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var morse = new Dictionary<string, char>()
+            {
+                {".-"   ,'A'},
+                {"-..." ,'B'},
+                {"-.-." ,'C'},
+                {"-.."  ,'D'},
+                {"."    ,'E'},
+                {"..-." ,'F'},
+                {"--."  ,'G'},
+                {"...." ,'H'},
+                {".."   ,'I'},
+                {".---" ,'J'},
+                {"-.-"  ,'K'},
+                {".-.." ,'L'},
+                {"--"   ,'M'},
+                {"-."   ,'N'},
+                {"---"  ,'O'},
+                {".--." ,'P'},
+                {"--.-" ,'Q'},
+                {".-."  ,'R'},
+                {"..."  ,'S'},
+                {"-"    ,'T'},
+                {"..-"  ,'U'},
+                {"...-" ,'V'},
+                {".--"  ,'W'},
+                {"-..-" ,'X'},
+                {"-.--" ,'Y'},
+                {"--.." ,'Z'}
+            };
+            string[] input = Console.ReadLine().Split(" | ");
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var word in input)
+            {
+                string[] characters = word.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var character in characters)
+                {
+
+                    result.Append(morse[character]);
+                }
+                result.Append(' ');
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
