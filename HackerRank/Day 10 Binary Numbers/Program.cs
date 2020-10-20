516fa00d07c3b2bbf5221cb0637f17430cbe7692
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-20 22:29:18 +0300

    update

---
+++ HackerRank/Day 10 Binary Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string binary = Convert.ToString(n, 2);
+
+        int maxConsecutive = 0;
+        int currentConsecutive = 0;
+
+        for (int i = 0; i < binary.Length; i++)
+        {
+            if (binary[i]== '1')
+            {
+                currentConsecutive++;
+            }
+            else
+            {
+                
+                if (currentConsecutive>maxConsecutive)
+                {
+                    maxConsecutive = currentConsecutive;
+                }
+
+                currentConsecutive = 0;
+            }
+        }
+
+        if (currentConsecutive > maxConsecutive)
+        {
+            maxConsecutive = currentConsecutive;
+        }
+
+        Console.WriteLine(maxConsecutive);
+    }
+}
