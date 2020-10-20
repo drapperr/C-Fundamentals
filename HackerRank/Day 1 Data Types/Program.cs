e0429c65af72bf8d9bf68f0404d86441666ce61b
DraPpeRr <drapperr@users.noreply.github.com>
2020-10-20 22:28:53 +0300

    update

---
+++ HackerRank/Day 1 Data Types/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+        int i2 = int.Parse(Console.ReadLine());
+        double d2 = double.Parse(Console.ReadLine());
+        string s2 = Console.ReadLine();
+
+        Console.WriteLine(i + i2);
+        Console.WriteLine($"{(d + d2):F1}");
+        Console.WriteLine(s + s2);
+    }
+}
